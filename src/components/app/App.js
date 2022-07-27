aaec6a36c2f4ee57b48bc2c2450abaab86428726
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-28 01:55:16 +0300

    import the Home component to the App component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Home from "../home/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Home />
       </header>
     </div>
   );
