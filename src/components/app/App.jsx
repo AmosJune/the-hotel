553c596b33049f559c8535030375175725b208e3
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-31 22:31:06 +0300

    comment out the Router tag

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Home from "../home/Home";
+// import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Home />
-      </header>
+    <div>
+      <Home />
     </div>
+    // <Router>
+    //   <div className="App">
+    //   </div>
+    //   hello
+    //   <Routes>
+    //     <Route exact path="/" element={<Home />}></Route>
+    //     {/* <Route exact path="/" element={} ></Route> */}
+    //     {/* <Route exact path="/search" element={<Search />}></Route> */}
+    //   </Routes>
+    // </Router>
   );
 }
 
 export default App;
-
