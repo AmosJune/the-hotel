3259e535e8a69b35088b41cdb2263b1dcd3b2a50
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-31 22:36:54 +0300

    insert the label and input tags to the file

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-function Search() {
-    return 
-};
+function Search({ handleSearch, search }) {
+  return (
+    <>
+      <label htmlFor="search" className="search-label">
+        Search
+      </label>
+      <input
+        type="search"
+        name="search"
+        id="search"
+        value={search}
+        onChange={handleSearch}
+      />
+    </>
+  );
+}
 
 export default Search;
